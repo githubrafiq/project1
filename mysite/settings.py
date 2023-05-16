13939d12308a3dd7572fe8e011b39b93855df2ff
rafiqul <githubrafiq@gmail.com>
2023-05-17 00:05:07 +0800

    requirements.txt runtime.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-import django-heroku
 import django_heroku
 import dj_database_url
 from decouple import config
